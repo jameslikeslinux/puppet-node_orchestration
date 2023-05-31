65f4a97f50d4f93db9c0c915e8d5a00fe54beefc
James Lee <james@james.tl>
2023-05-31 15:27:42 -0400

    Parameterize AMI settings

--- plans/launch_ec2_instance.pp
+++ plans/launch_ec2_instance.pp
@@ -2,17 +2,26 @@
 #
 # @param name The name of the instance to create
 # @param size The type of instance to create
-# @param region Overrides the default region expressed in Hiera
+# @param image_id Overrides the default AMI set in Hiera
+# @param ami_user Overrides the default AMI username set in Hiera
+# @param key_name Overrides the default SSH key name set in Hiera
+# @param region Overrides the default region set in Hiera
 plan node_orchestration::launch_ec2_instance (
   String $name,
   Enum['small', 'medium', 'large'] $size,
   Optional[String] $image_id = undef,
   Optional[String] $ami_user = undef,
+  Optional[String] $key_name = undef,
+  Optional[Variant[String, Array[String]]] $security_groups = undef,
+  Optional[String] $subnet   = undef,
   Optional[String] $region   = undef,
 ) {
   # Let defaults be defined in Hiera, overridden with parameters
   $real_image_id = pick($image_id, lookup('node_orchestration::ec2_image_id', Optional[String], 'first', undef))
   $real_ami_user = pick($ami_user, lookup('node_orchestration::ec2_ami_user', Optional[String], 'first', undef))
+  $real_key_name = pick($key_name, lookup('node_orchestration::ec2_key_name', Optional[String], 'first', undef))
+  $real_sgs      = pick($security_groups, lookup('node_orchestration::ec2_security_groups', Optional[Variant[String, Array[String]]], 'first', undef))
+  $real_subnet   = pick($subnet, lookup('node_orchestration::ec2_subnet', Optional[String], 'first', undef))
   $real_region   = pick($region, lookup('node_orchestration::ec2_region', Optional[String], 'first', undef))
 
   $api_token       = lookup('node_orchestration::api_token', String)
@@ -26,11 +35,11 @@ plan node_orchestration::launch_ec2_instance (
   run_task('aws::create_instance', $settings::server, 'Create the instance', {
     image_id        => $real_image_id,
     instance_type   => $instance_types[$size],
-    key_name        => 'test',
+    key_name        => $real_key_name
     name            => $name,
     region          => $real_region,
-    security_groups => 'test',
-    subnet          => 'test',
+    security_groups => $real_sgs,
+    subnet          => $real_subnet,
   })
 
   $hostname = Integer[0, 6].reduce(undef) |$result, $i| {
